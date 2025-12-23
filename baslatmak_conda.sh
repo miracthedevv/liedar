559d7026b565d410d71fd2c6f726f754e4d0b43a
Mirac Tahircan Yilmaz <muzik.kargasasi18@gmail.com>
2025-12-23 18:59:30 +0300

    Temizlik: Gereksiz dosyalar silindi

--- baslatmak_conda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Lie-Dar Conda Environment Başlatma Scripti
-
-# Conda environment'ı aktif et ve uygulamayı başlat
-source $HOME/miniconda3/etc/profile.d/conda.sh
-conda activate liedar
-python main.py
